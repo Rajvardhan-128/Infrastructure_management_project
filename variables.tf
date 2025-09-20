91e8d473ae7fbb0e762d4349acf6cec851363f72
Rajvardhan-128 <rajvardhankale284@gmail.com>
2025-09-20 23:42:18 +0530

    Updated_S3_bucket_name_02

--- variables.tf
+++ variables.tf
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-Terraform-"
+    default     = "my-s3bucket-terraform-"
 }
 variable "tags" {
     type        = map
